01031e94e106c14021683bf90977f2f34cd93efc
kaprisa57@gmail.com <kaprisa57@gmail.com>
2018-09-24 20:33:15 +0300

    - async

--- f.mjs
+++ f.mjs
@@ -7,58 +7,80 @@ class Parser {
     this.result = {}
     this.path = []
     this.mode = 'object'
+    this.remaining = ''
   }
 
-  parse(str) {
-    const matches = str.match(/[{}\[\]]|"[^"]*":?|\d+/ig)
-    this.process(matches)
+  async parse(str) {
+    const completeStr = this.remaining + str;
+    const matches = completeStr.match(/[{}\[\]]|"[^"]*":?|\d+/ig)
+    const last = matches[matches.length - 1]
+    this.remaining = completeStr.substring(completeStr.lastIndexOf(last) + last.length)
+    return this.process(matches)
   }
 
   set(v) {
     this.result = R.assocPath(this.path, v, this.result)
   }
 
+  get() {
+    return this.result
+  }
+
   process(matches) {
-    while (matches.length) {
-      const m = matches.shift()
-      if (m.endsWith(':')) {
-        const prop = m.replace(/[":]/ig, '')
-        const v = matches.shift()
-        this.path.push(prop)
-        if (v === '{') {
+    return new Promise((resolve) => {
+      while (matches.length) {
+        const m = matches.shift()
+        if (m.endsWith(':')) {
+          const prop = m.replace(/[":]/ig, '')
+          const v = matches.shift()
+          if (!v) {
+            this.remaining = m + this.remaining
+            resolve(true)
+            return
+          }
+          this.path.push(prop)
+          if (v === '{') {
+            this.mode = 'object'
+            this.set({})
+          } else if (v === '[') {
+            this.mode = 'array'
+            this.set([])
+            this.path.push(-1)
+          } else {
+            this.set(v.replace(/"/g, ''))
+            this.path.pop()
+          }
+        } else if (m === ']') {
+          this.path.pop()
+          this.path.pop()
           this.mode = 'object'
-          this.set({})
-        } else if (v === '[') {
-          this.mode = 'array'
-          this.set([])
-          this.path.push(-1)
-        } else {
-          this.set(v.replace(/"/g, ''))
+        } else if (this.mode === 'array') { // array
+          const idx = this.path.pop()
+          this.path.push(idx + 1)
+          if (m === '{') {
+            this.set({})
+          } else {
+            this.set(m.replace(/"/g, ''))
+          }
+        }
+        if (m === '}') {
           this.path.pop()
         }
-      } else if (m === ']') {
-        this.path.pop()
-        this.path.pop()
-        this.mode = 'object'
-      } else if (this.mode === 'array') { // array
-        const idx = this.path.pop()
-        this.path.push(idx + 1)
-        if (m === '{') {
-          this.set({})
-        } else {
-          this.set(m.replace(/"/g, ''))
-        }
       }
-      if (m === '}') {
-        this.path.pop()
-      }
-    }
+     resolve(true)
+    })
   }
 }
-const time = Date.now()
-const parser = new Parser()
-parser.parse(str)
-console.log(parser.result)
-console.log(Date.now() - time)
+
+(async () => {
+  const time = Date.now()
+  const parser = new Parser()
+  await parser.parse(str.substring(0, 53))
+  await parser.parse(str.substring(53))
+  console.log(JSON.stringify(parser.result).length)
+  console.log(str.length)
+  console.log(Date.now() - time)
+ })()
+
 
 
